8903fbd5338fd59d0dfcebe1040685df499603b5
MutNation <welpmut9@gmail.com>
2025-07-14 21:39:50 +0300

    Fix: Import and render Header, MainContent, Footer components in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,5 +1,8 @@
 import UserProfile from './components/UserProfile';
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
@@ -10,14 +13,9 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <Header />
+      <MainContent />
+      <Footer />
 
       <WelcomeMessage />
 
@@ -26,6 +24,15 @@ function App() {
         age="25" 
         bio="Loves hiking and photography" 
       />
+
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
 
       <h1>Vite + React</h1>
       <div className="card">
