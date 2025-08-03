bd9d201982b3f012cb744b69dfaa20a12a32ce6c
MutNation <welpmut9@gmail.com>
2025-08-03 20:13:51 +0300

    Rename SearchForm to Search and finalize task 1 requirements

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,22 +1,38 @@
+
 import { useState } from 'react';
-import SearchForm from './components/SearchForm';
+import Search from './components/Search';
 import UserCard from './components/UserCard';
 import { fetchUserData } from './services/githubService';
 
 function App() {
   const [user, setUser] = useState(null);
   const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false); // optional but improves UX
 
   const handleSearch = async (username) => {
     try {
+      setIsLoading(true);
       setError('');
       const userData = await fetchUserData(username);
       setUser(userData);
     } catch (err) {
       setUser(null);
-      setError(err.message);
+      setError('Looks like we can’t find the user');
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>GitHub User Search</h1>
+      <Search onSearch={handleSearch} />
+
+      {isLoading && <p>Loading...</p>}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {user && <UserCard user={user} />}
+    </div>
+  );
+}
+
+export default App;
